ffdea0641a7bd92d683c2f01cb8a53e489b6a89f
yannco <wikifixcom@gmail.com>
2024-10-23 20:08:45 +0100

    bug

--- sources/game_render/raycaster_map/draw_duck.c
+++ sources/game_render/raycaster_map/draw_duck.c
@@ -6,7 +6,7 @@
 /*   By: yadereve <yadereve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 16:05:51 by yadereve          #+#    #+#             */
-/*   Updated: 2024/10/23 16:39:44 by yadereve         ###   ########.fr       */
+/*   Updated: 2024/10/23 20:05:08 by yadereve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,30 +48,77 @@ t_point	set_texture_start_position(double y, int width)
 	return (tex);
 }
 
+void	animation_duck()
+{
+	struct timeval			*last_frame_time = NULL;
+
+
+	if (time_over(*last_frame_time, 150000))
+	{
+		if (curent->next)
+		{
+			curent = curent->next;
+			if (data->temp_type == BACKPACK)
+				curent_backpack = curent;
+			else if (data->temp_type == DUCK)
+				curent_duck = curent;
+		}
+		else
+		{
+			if (data->temp_type == BACKPACK)
+				curent_backpack = data->textures->backpack;
+			else if (data->temp_type == DUCK)
+				curent_duck = data->textures->duck;
+		}
+		set_timer(last_frame_time);
+	}
+}
+
 void	draw_duck(t_data *data, int x, double distance_to_duck)
 {
 	int						duck_height;
 	t_point					tex;
-	static struct timeval	last_frame_time;
-	static t_image			*curent = NULL;
+	static struct timeval	duck_frame_time;
+	static struct timeval	backpack_frame_time;
+	// struct timeval			*last_frame_time = NULL;
+	static t_image			*curent_duck = NULL;
+	static t_image			*curent_backpack = NULL;
+	t_image					*curent;
 
-	if (curent == NULL)
+	if (curent_duck == NULL)
+		curent_duck = data->textures->duck;
+	if (curent_backpack == NULL)
+		curent_backpack = data->textures->backpack;
+	if (data->temp_type == BACKPACK)
 	{
-		if (data->temp_type == BACKPACK)
-			curent = data->textures->backpack;
-		else if (data->temp_type == DUCK)
-			curent = data->textures->duck;
+		last_frame_time = &backpack_frame_time;
+		curent = curent_backpack;
 	}
-	if (time_over(last_frame_time, 150000))
+	else if (data->temp_type == DUCK)
 	{
-		if (curent->next)
-			curent = curent->next;
-		else if (data->temp_type == BACKPACK)
-			curent = data->textures->backpack;
-		else if (data->temp_type == DUCK)
-			curent = data->textures->duck;
-		set_timer(&last_frame_time);
+		last_frame_time = &duck_frame_time;
+		curent = curent_duck;
 	}
+	// if (time_over(*last_frame_time, 150000))
+	// {
+	// 	if (curent->next)
+	// 	{
+	// 		curent = curent->next;
+	// 		if (data->temp_type == BACKPACK)
+	// 			curent_backpack = curent;
+	// 		else if (data->temp_type == DUCK)
+	// 			curent_duck = curent;
+	// 	}
+	// 	else
+	// 	{
+	// 		if (data->temp_type == BACKPACK)
+	// 			curent_backpack = data->textures->backpack;
+	// 		else if (data->temp_type == DUCK)
+	// 			curent_duck = data->textures->duck;
+	// 	}
+	// 	set_timer(last_frame_time);
+	// }
+
 	duck_height = (int)(data->screen_height / (distance_to_duck));
 	tex = set_texture_start_position(data->fraction.y, curent->width);
 	draw_textured_line(curent, x, duck_height, tex);
